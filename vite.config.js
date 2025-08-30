546a7c4fe852cb2b36e31c98c3de8d543f6743db
arnabdutta <arnabdutta0x0@gmail.com>
2025-08-30 10:44:39 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/Arnab-Portfolio/',
+  base: '/Arnab-Portfolio/',
   plugins: [
-    react(), 
+    react(),
     tailwindcss()
   ],
 })
