2ebe01b64d85e7ccaf0f5b650d9f53238303c0d9
arnabdutta03 <arnabkumardutta03@gmail.com>
2026-08-30 07:32:10 +0530

    update the package json for vercel

---
+++ readmeASAP.txt
@@ -0,0 +1,3 @@
+"homepage": "https://arnabdutta03.github.io/Arnab-Portfolio",
+
+add this to package.json to host it in web
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useEffect, useState } from "react";
 
-
 function About() {
   const [isMobile, setIsMobile] = useState(window.innerWidth < 640);
 
